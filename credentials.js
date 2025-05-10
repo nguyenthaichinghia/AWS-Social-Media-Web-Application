d6a235efe50602cf5d8ddbfe17614505d1a7ae48
nguyenthaichinghia <nghia.nguyenthaichi2004@gmail.com>
2025-05-10 11:47:42 +0700

    Minor changes

---
+++ credentials.js
@@ -0,0 +1,5 @@
+export const DB_HOST = <YOUR_DATABASE_HOST>;
+export const DB_USER = <YOUR_DATABASE_USERNAME>;
+export const DB_PASSWORD = <YOUR_DATABASE_PASSWORD>;
+export const DB_NAME = <YOUR_DATABASE_TABLE_NAME>;
+export const EC2_URL = 'http://<YOUR_EC2_PUBLIC_IP:YOUR_EC2_PUBLIC_PORT>/posts' <>;
